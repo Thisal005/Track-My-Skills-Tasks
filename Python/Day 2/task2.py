241ba366162471f516e44163001cb8d58c7d3c65
Thisal005 <thisalth2@gmail.com>
2026-04-22 21:32:37 +0530

    Add odd/even checker script

---
+++ Python/Day 2/task2.py
@@ -0,0 +1,9 @@
+print("Odd/Even Checker")
+
+num = int(input("Enter your Number : "))
+
+ans = num % 2
+if ans == 1:
+    print(f"The {num} is a Odd Numebr")
+else:
+    print(f"The {num} is a Even Numebr")
