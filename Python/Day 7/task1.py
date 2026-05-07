a49b74eb5bb2afdb08796adb3b1d4192c3ea8371
Thisal005 <thisalth2@gmail.com>
2026-05-07 11:50:06 +0530

    Add script to find the factorial of a number

---
+++ Python/Day 7/task1.py
@@ -0,0 +1,11 @@
+# Find factorial of a number
+
+num = int(input("Enter number to find the factorial : "))
+
+def factorial(n):
+    fact = 1
+    for i in range(1,n+1):
+        fact *= i
+    print(fact)
+
+factorial(num)
